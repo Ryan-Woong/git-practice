e497a3aa97bb25e38ad65a3477e77dc51c3bc27c
Ryan-Woong <starlord10320@gmail.com>
2023-10-18 19:23:39 +0900

    git test 3

---
+++ Arduino_music_scale/_____________.ino
@@ -0,0 +1,19 @@
+
+
+void setup() {
+  // put your setup code here, to run once:
+int speakerpin = 9; //스피커가 연결된 디지털핀 설정
+int note[] = {2093,2349,2637,2793,3136,3520,3951,4186}; //도레미파솔라시도
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ // put your setup code here, to run once:
+  int elementCount = sizeof(note) / sizeof(int);
+  for (int i=0; i < elementCount; i++)//note를 play
+    {
+    tone(speakerpin,note[i],500);
+    delay(750);
+  }
+}
